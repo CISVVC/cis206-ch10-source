bff9aa39a273bc1e971c03b3a67880e13163042b
Paul Tonning <tonningp@users.noreply.github.com>
2018-05-10 11:04:55 -0700

    Add files via upload

---
+++ Simple/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+      final int FRAME_WIDTH = 800;
+      final int FRAME_HEIGHT = 800;
+      frame.setSize(FRAME_WIDTH,FRAME_HEIGHT);
+      frame.setTitle("My first frame");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
+}
+
